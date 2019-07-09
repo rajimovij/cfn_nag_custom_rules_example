2c724022a9bd9025e8b182b36571979f44ac1cc2
rajimovij <52269337+rajimovij@users.noreply.github.com>
2019-07-09 10:43:24 -0600

    Update S3EncryptBool.rb

--- lib/rules/S3EncryptBool.rb
+++ lib/rules/S3EncryptBool.rb
@@ -19,8 +19,9 @@ class S3BucketEncryptionViolationRule < BaseRule
   def audit_impl(cfn_model)
     violating_buckets = cfn_model.resources_by_type('AWS::S3::Bucket').select do |bucket|
       violating_bucketencryptions = bucket.BucketEncryption.select do |bucketencryptions|
+        bucket.bucketencryptions.nil?
           end
-          violating_bucketencryptions.empty?
+          !violating_bucketencryptions.empty?
     end
 
     violating_buckets.map(&:logical_resource_id) + violating_bucketencryptions.map(&:logical_resource_id)
